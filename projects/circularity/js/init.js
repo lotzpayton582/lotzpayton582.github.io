b0195a857bdbfe08ae821f6281e18c81d1967ec4
lotzpayton582 <lotz582@wearescpps.org>
2024-11-12 19:52:30 +0000

    circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -21,12 +21,23 @@ var init = function (window) {
         
         // TODO 1 : Declare and initialize our variables
 
+var circle; // variable to hold a single circle when creating circles / iterating
+var circles = []; // variable to store all circles in one Array
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            // Code to draw a circle
+circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+physikz.addRandomVelocity(circle, canvas, 5 ,5);
+view.addChild(circle);
+circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
 
+    for(var loops = 0; loops < 100; loops++){
+        drawCircle();
+    }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,10 +50,17 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+           for(var i = 0; i < circles.length; i++){
+ 
+            physikz.updatePosition(circles[i]);
+            game.checkCirclePosition(circles[i]);
+           }
+            
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
+           
+            
 
             // TODO 9 : Iterate over the array
            
@@ -63,8 +81,15 @@ var init = function (window) {
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
-
-
+            if ( circle.x < 0) {
+                circle.x = canvas.width 
+            }
+            if ( circle.y > canvas.height) {
+                circle.y = 0;
+            }
+            if ( circle.y < 0) {
+                circle.y = canvas.height;
+            }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
